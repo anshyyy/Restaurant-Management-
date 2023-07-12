e94eea58c00d287488c65c16703e25cdd8c4d256
Anshuman Sharma <anshuman9998@gmail.com>
2023-07-12 21:15:49 +0530

    added new client

--- client/assets/javascript/search.js
+++
@@ -1,9 +0,0 @@
-const explore = document.querySelector(".btn-explore");
-const addplate= document.querySelectorAll(".btn-addToPlate");
-//button clicking function defined
-const fun = function () {
-  window.open("explore.html", "_self");
-};
-
-explore.addEventListener("click", fun);
-
